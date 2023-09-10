ffa978f9cafef47583e50b9f311387deb09c733e
PC <kerik006@mail.ru>
2023-09-10 22:45:17 +0300

    homework

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers
+(
+	customer_id CHAR PRIMARY KEY,
+	company_name CHARACTER VARYING,
+	contact_name CHARACTER VARYING
+);
+
+CREATE TABLE employees
+(
+	employee_id INT PRIMARY KEY,
+	first_name CHARACTER VARYING,
+	last_name CHARACTER VARYING,
+	title CHARACTER VARYING,
+	birth_date DATE,
+	notes CHARACTER VARYING
+);
+
+CREATE TABLE orders
+(
+	order_id INT PRIMARY KEY,
+	customer_id CHAR REFERENCES customers(customer_id),
+	employee_id INT REFERENCES employees(employee_id),
+	order_date DATE,
+	ship_city CHARACTER VARYING
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,18 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+from multiprocessing import connection
+
+import psycopg2
+import pandas as pd
+
+if __name__ == 'main':
+    conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="kerik00989A"
+    )
+
+
+
+
+
