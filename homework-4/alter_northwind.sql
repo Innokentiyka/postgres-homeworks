da343e632d70020c523f2b3e3264c04834d77ea8
PC <kerik006@mail.ru>
2023-09-17 11:54:28 +0300

    homework 4

--- homework-4/alter_northwind.sql
+++ homework-4/alter_northwind.sql
@@ -1,12 +1,36 @@
 -- Подключиться к БД Northwind и сделать следующие изменения:
 -- 1. Добавить ограничение на поле unit_price таблицы products (цена должна быть больше 0)
+DELETE FROM products WHERE unit_price <= 0;
+ALTER TABLE products
+ADD CONSTRAINT CK_UnitPrice_Positive CHECK (unit_price > 0);
 
 
 -- 2. Добавить ограничение, что поле discontinued таблицы products может содержать только значения 0 или 1
+SELECT * FROM products WHERE discontinued NOT IN (0, 1);
+DELETE FROM products WHERE discontinued NOT IN (0, 1);
 
+
+ALTER TABLE products
+ADD CONSTRAINT CK_Discontinued_Values CHECK (discontinued IN (0, 1));
 
 -- 3. Создать новую таблицу, содержащую все продукты, снятые с продажи (discontinued = 1)
-
+CREATE TABLE DiscontinuedProducts AS
+SELECT *
+FROM products
+WHERE discontinued = 1;
 
 -- 4. Удалить из products товары, снятые с продажи (discontinued = 1)
 -- Для 4-го пункта может потребоваться удаление ограничения, связанного с foreign_key. Подумайте, как это можно решить, чтобы связь с таблицей order_details все же осталась.
+CREATE TEMP TABLE Temp_DiscontinuedProducts AS
+SELECT *
+FROM products
+WHERE discontinued = 1;
+
+DELETE FROM products WHERE discontinued = 1;
+
+UPDATE order_details
+SET product_id = p.product_id
+FROM Temp_DiscontinuedProducts p
+WHERE order_details.product_id = p.product_id;
+
+DROP TABLE Temp_DiscontinuedProducts;
--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -1,19 +1,35 @@
 -- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
-
+CREATE TABLE student (
+    student_id serial PRIMARY KEY,
+    first_name varchar,
+    last_name varchar,
+    birthday date,
+    phone varchar
+);
 
 -- 2. Добавить в таблицу student колонку middle_name varchar
-
+ALTER TABLE student
+ADD COLUMN middle_name varchar;
 
 -- 3. Удалить колонку middle_name
-
+ALTER TABLE student
+DROP COLUMN middle_name;
 
 -- 4. Переименовать колонку birthday в birth_date
-
+ALTER TABLE student
+RENAME COLUMN birthday TO birth_date;
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
-
+ALTER TABLE student
+ALTER COLUMN phone TYPE varchar(32);
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
-
+INSERT INTO student (first_name, last_name, birth_date, phone)
+VALUES
+    ('Кирилл', 'Алёшин', '2002-02-12', '123-456-789'),
+    ('Абдурахман', 'Ла-Ахад', '1998-05-15', '987-654-321'),
+    ('Матиас', 'Риверра', '2002-09-30', '555-555-555');
 
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
+DELETE FROM student;
+ALTER SEQUENCE student_student_id_seq RESTART WITH 1;
